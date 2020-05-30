828d2adc6c5e339ba4403034a3f9206e4190b85a
nobuyori-gh <vcd02nm120jtik@gmail.com>
2020-05-30 16:10:49 +0900

    js.8

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -9,14 +9,29 @@
 // 	});
 // });
 
-$(function(){
-	$('.box1').slideDown(function(){
-		$('.box1').css({
-		'background-color':'#0000ff',
-		'width': '200px',
-		'height':'100px'
-	}).slideUp();
-	});
+// $(function(){
+// 	$('.box1').slideDown(function(){
+// 		$('.box1').css({
+// 		'background-color':'#0000ff',
+// 		'width': '200px',
+// 		'height':'100px'
+// 	}).slideUp();
+// 	});
 	// $('.box1').show();
 	// $('.box1').hide();
+// });
+
+$(function(){
+	$('button').on('click',function(){
+		$('ul').children().css('color','red');
+	});
+	// $('.bg2').on('click',function(){
+	// 	$('.bg2').slideUp();
+	// });
+	// $('.bg3').on('click',function(){
+	// 	$('.bg3').slideUp();
+	// });
+	// $('.bg4').on('click',function(){
+	// 	$('.bg4').slideUp();
+	// });
 });
